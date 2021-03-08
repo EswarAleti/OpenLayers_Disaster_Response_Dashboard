595c2b6a5e3a3f27acb083cd2901149e4a315b96
Srinivasarao Aleti <asrinivasarao@woundtech.net>
2021-03-08 17:17:28 +0530

    First commit

---
+++ Annotations_OpenLayers/index.js
@@ -0,0 +1,37 @@
+import 'ol/ol.css';
+import {Map, View, Feature} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import * as olLayer from  'ol/layer';
+import * as olSource from 'ol/source';
+import {Point} from 'ol/geom';
+import OSM from 'ol/source/OSM';
+import * as olCoordinate from 'ol/coordinate';
+import {toLonLat,fromLonLat} from 'ol/proj';
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: fromLonLat([79.4192,13.6288]),
+    zoom: 8
+  })
+});
+
+var layer = new olLayer.Vector({
+     source: new olSource.Vector({
+         features: [
+             new Feature({
+                 geometry: new Point(fromLonLat([79.4192,13.6288]))
+             })
+         ]
+     })
+ });
+ map.addLayer(layer);
+
+map.on('click',function(e){
+  console.log('Longitude: '+toLonLat(e.coordinate)[0]+' Lattitude: '+toLonLat(e.coordinate)[1]);
+})
